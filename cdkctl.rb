994c97ccc9cd32d3a1ccceb8612a95047099fe53
maheshrayas <maheshrayas@gmail.com>
2020-11-29 19:47:03 +1100

    Update version 0.2.0

--- cdkctl.rb
+++ cdkctl.rb
@@ -3,7 +3,7 @@ class Cdkctl < Formula
     homepage "https://github.com/maheshrayas/cdkctl"
     url "https://github.com/maheshrayas/cdkctl/releases/download/v.0.2.0/darwin-cdkctl.tar.gz"
     sha256 "4b9dff0baf587683dcc60fd73a55ea2b9590768bd2172dfaef99f3855bf3af0f"
-    version "0.1.0"
+    version "0.2.0"
   
   
     bottle :unneeded
