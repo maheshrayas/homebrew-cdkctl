e2aff37904eb6f2128400a3b81ac7c4fa90aa1b8
maheshrayas <maheshrayas@gmail.com>
2020-11-29 19:40:12 +1100

    v0.2.0

--- cdkctl.rb
+++ cdkctl.rb
@@ -1,8 +1,8 @@
 class Cdkctl < Formula
     desc "A command line tool to deploy cdk stacks concurrently"
     homepage "https://github.com/maheshrayas/cdkctl"
-    url "https://github.com/maheshrayas/cdkctl/releases/download/v.0.1.0/darwin-cdkctl.tar.gz"
-    sha256 "60b2d76ca0d09ec0f22135671738565c89f93774e8ce4f1002fdb114a59d7e9d"
+    url "https://github.com/maheshrayas/cdkctl/releases/download/v.0.2.0/darwin-cdkctl.tar.gz"
+    sha256 "4b9dff0baf587683dcc60fd73a55ea2b9590768bd2172dfaef99f3855bf3af0f"
     version "0.1.0"
   
   
